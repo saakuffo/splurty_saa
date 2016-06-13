2f226d893a0b2d4919fa1b3e824cc0b74ccaea30
Stephen Akuffo <slyven@gmail.com>
2016-06-13 16:44:30 +0000

    Adjusted production.rb so it compiles assests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ SplurtySaa::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true #changed from default [TFH said do it]
 
   # Generate digests for assets URLs.
   config.assets.digest = true
