30befb4104fb7d75b287a2ff6d2f67f9e518d347
Stephen Akuffo <slyven@gmail.com>
2016-11-17 17:46:20 +0000

    codecov

--- spec/models/quotes_spec.rb
+++ spec/models/quotes_spec.rb
@@ -1,19 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Quote do
-  
+
   it "is not valid without a saying and an author" do
     quote = Quote.new
     expect(quote).not_to be_valid
   end
 
-  it "saying should not be longer than 140 characters" do 
+  it "saying should not be longer than 140 or shorter than 3 characters" do 
     quote = Quote.new(saying: 'a' * 141, author: 'a' * 3 )
+    expect(quote).not_to be_valid
+
+    quote = Quote.new(saying: 'a' * 2, author: 'a' * 3 )
     expect(quote).not_to be_valid
   end
 
-  it "author's name should not be longer than 50 characters" do
+  it "author's name should not be longer than 50 or shorter than 3 characters" do
     quote = Quote.new(saying: 'a' * 50, author: 'a' * 51 )
     expect(quote).not_to be_valid
+
+    quote = Quote.new(saying: 'a' * 50, author: 'a' * 2 )
+    expect(quote).not_to be_valid
+  end
+
+  it "Quote should be valid if attributes and exist and length rules are not broken" do
+    quote = Quote.new(saying: 'a' * 140, author: 'a' * 50 )
+    expect(quote).to be_valid
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'database_cleaner'
 require 'capybara/rspec'
+require 'simplecov'
+require 'codecov'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -28,6 +30,8 @@ require 'capybara/rspec'
 ActiveRecord::Migration.check_pending!
 
 RSpec.configure do |config|
+  SimpleCov.start
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
